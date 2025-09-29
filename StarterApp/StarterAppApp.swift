a839ec2e05c8c61c8870c8bb95299a20aeb43364
Hozzi <dmitriylazebnyy911@gmail.com>
2025-09-29 23:28:03 +0300

    Initial Commit

---
+++ StarterApp/StarterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarterAppApp.swift
+//  StarterApp
+//
+//  Created by Dmitry Lazebnyi on 29.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct StarterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
